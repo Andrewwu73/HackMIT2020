4f2e835d8677fb51b3f801a98730bb2aad8659d2
flyingtacov42 <raymond.b.li8@gmail.com>
2020-09-20 00:04:19 -0700

    edits to index.js

--- react-app/src/views/about/AboutView/index.js
+++ react-app/src/views/about/AboutView/index.js
@@ -38,16 +38,16 @@ const Account = () => {
 
         <Typography variant="h4" gutterBottom> 2. Machine Learning </Typography>
         <Typography variant="body1" gutterBottom>
-          To analyze the datasets, we took a variety of approaches. On the landing page, you can see a linear forecast of the GDPs
+          To analyze the datasets, we took a variety of approaches. On the home page, you can see a forecast of the GDPs
           in the near future. This forecast was generated from an autoregressive integrated moving average model, a machine learning
           algorithm that makes correlations between GDP and time in order to predict how the GDP will change in future years.
           
           While powerful, the ARIMA model requires the data to be to be parameterized by time which prevented us from being able to
           do explore correlations in the data unrelated to time. To analyze the datasets in greater detail, we developed our own
           Machine Learning model. This model take as input a series of five economic indicators of a country: GDP, population size,
-          average work hours, education index and youth mortality index. From this input, the model uses a recurive neural network
-          to predict the values of these indicators in the following year. This model was trained using data on these indicators from
-          University of Groningen, and Our World in Data on a mean square loss function. Using this model, we can make predictions
+          average work hours, education index and youth mortality rate. From this input, the model uses a recursive neural network
+          to predict the values of these indicators in the following year. This model was trained using data on these indicators from the
+          University of Groningen and Our World in Data. Using this model, we can make predictions
           arbitrarily far into the future by recursively propagating these indicators through the model.
           (autoregressive)
         </Typography>
