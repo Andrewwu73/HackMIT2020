227e2bbbddbe025c8ef9d1ad10e3993427eee05f
anthonyou <67976437+anthonyou@users.noreply.github.com>
2020-09-20 00:18:18 -0400

    Update model.py

--- API/model.py
+++ API/model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
+from API import preprocessing
 
 class Propagator(nn.Module):
     """
@@ -15,9 +16,11 @@ class Propagator(nn.Module):
         self.output = nn.Linear(n_hidden, n_output)
 
     def forward(self, x):
-        x = F.elu(self.hidden1(x))
+        x = torch.transpose(x, 0, 1)
+        x = F.relu(self.hidden1(x))
         x = F.relu(self.hidden2(x))
         x = self.output(x)
+        x = torch.transpose(x, 0, 1)
         return x
 
 
@@ -48,25 +51,28 @@ def country_loss(pred_evo, act_evo):
     loss += torch.mean((pred_evo[0,:] - act_evo[0,:])**2)
     return loss
 
+files = ["GDP.csv", "population.csv", "hours_worked.csv", "education.csv", "youth-mortality-rate.csv"]
+varnames = ["GDP", "population", "hours worked", "education index", "youth mortality rate"]
+loaded_data = preprocessing.parse_data(files, varnames)
 
-data = {'USA': [[2020, 100], [2021, 80], [2022, 23]]}
-
-d = 1
+d = len(loaded_data[0]) - 1
+data = loaded_data[1]
 model = Regression(d, d)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 N = 500
 for i in range(N):
     overall_loss = 0
     for country in data.keys():
-        country_data = data[country]
-        act_evo = torch.tensor(country_data)[:,1:]
-        act_evo = torch.transpose(act_evo, 0, 1)
-        x = act_evo[:,0:1]
-        T = act_evo.size()[1]
-        x = Variable(x.float(), requires_grad=True)
-        pred_evo = model(x, T)
-        overall_loss += country_loss(pred_evo, act_evo)
+        if len(data[country]) > 0:
+            country_data = data[country]
+            act_evo = torch.tensor(country_data)[:,1:]
+            act_evo = torch.transpose(act_evo, 0, 1)
+            x = act_evo[:,0:1]
+            T = act_evo.size()[1]
+            x = Variable(x.float(), requires_grad=True)
+            pred_evo = model(x, T)
+            overall_loss += country_loss(pred_evo, act_evo)
     print(overall_loss)
 
     optimizer.zero_grad()
