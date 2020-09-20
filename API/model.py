2931aefc816aa6728d7dc2af68dbabf96d71ddc4
anthonyou <67976437+anthonyou@users.noreply.github.com>
2020-09-19 21:30:24 -0400

    this the shit

--- API/model.py
+++ API/model.py
@@ -1,4 +1,16 @@
-import tensorflow
-from tensorflow import keras
-from tensorflow.keras import layers
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Propagator(nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Propagator, self).__init__()
+        self.hidden = nn.Linear(n_feature, n_hidden)
+        self.output = nn.Linear(n_hidden, n_output)
+
+    def forward(self, x):
+        x = F.Sigmoid(self.hidden(x))
+        x = F.Relu.output(x)
+        return x
+
 
