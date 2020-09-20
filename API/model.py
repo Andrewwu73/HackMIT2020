e73521dfef35756b04d9de82e885b3775735639a
flyingtacov42 <raymond.b.li8@gmail.com>
2020-09-19 22:57:50 -0700

    fixed nn

--- API/model.py
+++ API/model.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 from API import preprocessing
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Propagator(nn.Module):
     """
@@ -35,11 +37,11 @@ class Regression(nn.Module):
         self.prop = Propagator(n_feature, n_hidden, n_output)
 
     def forward(self, x, T):
+        """T is number of years"""
         country_data = x
-        for j in range(T-1):
+        for j in range(T):
             x = self.prop(x)
-            country_data = torch.cat((country_data, x), 1)
-        return country_data
+        return x
 
 
 def country_loss(pred_evo, act_evo):
@@ -48,7 +50,7 @@ def country_loss(pred_evo, act_evo):
     the loss for that country.
     """
     loss = 0
-    loss += torch.mean((pred_evo[0,:] - act_evo[0,:])**2)
+    loss += ((pred_evo[0, 0] - act_evo[0])/act_evo[0])**2
     return loss
 
 files = ["GDP.csv", "population.csv", "hours_worked.csv", "education.csv", "youth-mortality-rate.csv"]
@@ -58,9 +60,9 @@ loaded_data = preprocessing.parse_data(files, varnames)
 d = len(loaded_data[0]) - 1
 data = loaded_data[1]
 model = Regression(d, d)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
 
-N = 500
+N = 100
 for i in range(N):
     overall_loss = 0
     for country in data.keys():
@@ -68,15 +70,37 @@ for i in range(N):
             country_data = data[country]
             act_evo = torch.tensor(country_data)[:,1:]
             act_evo = torch.transpose(act_evo, 0, 1)
-            x = act_evo[:,0:1]
-            T = act_evo.size()[1]
-            x = Variable(x.float(), requires_grad=True)
-            pred_evo = model(x, T)
-            overall_loss += country_loss(pred_evo, act_evo)
-    print(overall_loss)
-
+            for i in range(act_evo.shape[0] - 1):
+                time = country_data[i+1][0] - country_data[i][0]
+                x = act_evo[:, i: i+1]
+                x = Variable(x.float(), requires_grad=True)
+                pred_evo = model(x, time)
+                # print ("pred_evo: " + str(pred_evo))
+                # print ("act_evo: " + str(act_evo[:, i+1]))
+                # print (pred_evo.shape)
+                overall_loss += country_loss(pred_evo, act_evo[:, i+1])
+    print("overall loss: " + str(overall_loss))
+    # print (type(overall_loss))
     optimizer.zero_grad()
     overall_loss.backward()
     optimizer.step()
-final_pred = model(x,T)
-print(final_pred)
+# for param in model.parameters():
+#     print (param.data)
+
+gdp_act, gdp_pred = [], []
+for country in data:
+    country_data = data[country]
+    if not country_data:
+        continue
+    act_evo = np.array(country_data)[:, 1:]
+    act_evo = np.transpose(act_evo)
+    for i in range(act_evo.shape[0] - 1):
+        time = country_data[i + 1][0] - country_data[i][0]
+        x = torch.tensor(act_evo[:, i: i + 1])
+        x = Variable(x.float(), requires_grad=True)
+        pred_evo = model(x, time)
+        gdp_act.append(country_data[i][1])
+        gdp_pred.append(pred_evo[0, 0].item())
+
+# plt.scatter(gdp_act, gdp_pred, size=5)
+# plt.show()
